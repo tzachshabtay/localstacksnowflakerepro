d6aeb1930202a188eafe8b7bd524bf68e2615abe
Tzach Shabtay <tzachshabtay@gmail.com>
2025-05-16 21:53:11 +0000

    timestamp repro

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 import snowflake.connector as sf
-
-with open('/tmp/myfile.csv', 'w') as f:
-    f.write('ID,NAME\n')
-    f.write('1,20210101\n')
+import json
 
 conn = sf.connect(
     user="test",
@@ -16,22 +13,40 @@ conn = sf.connect(
 
 cursor = conn.cursor()
 
-cursor.execute("PUT file:///tmp/myfile.csv @MYSCHEMA.MYSTAGE/path/to/stage")
+source_data = [
+    {"COL1": "A", "COL2": "2023-01-01", "COL3": "X"},
+]
 
 cursor.execute("""
-MERGE INTO MYSCHEMA.MYTABLE AS TARGET
-USING (
+WITH Source AS (
     SELECT
-        $1 AS ID,
-        $2 AS NAME
-    FROM @MYSCHEMA.MYSTAGE/path/to/stage
-    (FILE_FORMAT => MYSCHEMA.MYFORMAT)
-) AS NEW_ROWS
-ON TARGET.ID = NEW_ROWS.ID
-WHEN NOT MATCHED THEN
-    INSERT (ID, NAME)
-    VALUES (NEW_ROWS.ID, NEW_ROWS.NAME)
-""")
+        VALUE:COL1::STRING AS COL1,
+        VALUE:COL2::DATE AS COL2,
+        VALUE:COL3::STRING AS COL3
+    FROM TABLE(FLATTEN(INPUT => PARSE_JSON(%s)))
+)
+SELECT
+    COALESCE(cm.COL1, '') AS COL1,
+    COALESCE(TO_CHAR(cm.COL2, 'YYYYMMDD')::INT, 0) AS COL2,
+    COALESCE(cm.COL3, '') AS COL3
+FROM
+    Source cm
+LEFT JOIN MYSCHEMA.MYTABLE pm
+    ON cm.COL1 = pm.COL1
+    AND cm.COL2 = pm.COL2
+    AND cm.COL3 = pm.COL3
+LEFT JOIN MYSCHEMA.MYTABLE2 mc
+    ON cm.COL1 = mc.COL1
+    AND cm.COL2 = mc.COL2
+    AND cm.COL3 = mc.COL3
+GROUP BY
+    cm.COL1,
+    cm.COL2,
+    cm.COL3
+HAVING
+    COUNT(pm.COL4) = 10
+    AND COUNT(mc.COL4) = 0;
+""", (json.dumps(source_data),))
 
 conn.rollback()
 conn.close()
